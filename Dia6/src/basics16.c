616d9746d187ed9d9a1acb82528b5cfa88cf2b81
Kyle Jones <jonesk26@gmail.com>
2026-01-13 02:13:44 -0600

    Starting structs but passing out

---
+++ Dia6/src/basics16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Introduction to structs
+
+struct Coordinate
+{
+    /* data */
+
+    int x;
+    int y;
+    int z;
+
+};
+
+
+int main() {
+    struct Coordinate c1;
+    return 0;
+    
+}
